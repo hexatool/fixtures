06e5c67ca9e3da08cd7087fd4f59a1bf2f757eea
MarioMH8 <mmenendez@magnus-strategy.com>
2022-10-16 16:16:43 +0200

    chore: added build

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	build: {
+		target: "ESNext",
+		lib: {
+			formats: ["es", "cjs"],
+			fileName: (format) => `hexatool-fixtures.${format === "es" ? "mjs" : "cjs"}`,
+			entry: resolve(__dirname, 'src/index.ts'),
+		},
+		sourcemap: true,
+		rollupOptions: {
+			external: [
+				"node:path",
+				"fs-extra",
+				"globby",
+				"signal-exit",
+				"tempy",
+			],
+			output: {
+				exports: "named"
+			}
+		},
+	},
+});
